60a3d0426a5da301c13cd6b3a0bd9bf1671393dd
Joseph Liu <drowsy.liu@gmail.com>
2025-08-13 15:43:48 +0800

    use google font online

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,49 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <router-view />
+  <div aria-label="A complete example of page header">
+    <el-page-header>
+      <template #content>
+        <div class="flex items-center">
+          <el-avatar
+            class="mr-3"
+            :size="50"
+            src="https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png"
+          />
+          <span class="text-large font-600 mr-3"> Title </span>
+        </div>
+      </template>
+      <template #extra>
+        <div class="flex items-center">
+          <el-button class="ml-2" plain>Aug-13-2025</el-button>
+        </div>
+      </template>
 
-  <footer>
-    <p>Made with ❤️ by <a href="" target="_blank" rel="noopener noreferrer">Your Name</a></p>
-  </footer>
+      <el-descriptions :column="3" size="small" class="mt-4">
+        <el-descriptions-item label="Username"> kooriookami </el-descriptions-item>
+        <el-descriptions-item label="Telephone"> 18100000000 </el-descriptions-item>
+        <el-descriptions-item label="Place">Suzhou</el-descriptions-item>
+        <el-descriptions-item label="Remarks">
+          <el-tag size="small">School</el-tag>
+        </el-descriptions-item>
+        <el-descriptions-item label="Address">
+          No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
+        </el-descriptions-item>
+      </el-descriptions>
+      <p class="mozila-headline-700 mt-4 text-large">
+        Element Plus team uses <b>weekly</b> release strategy under normal circumstance, but
+        critical bug fixes would require hotfix so the actual release number <b>could be</b> more
+        than 1 per week.
+      </p>
+    </el-page-header>
+  </div>
 </template>
 
-<style scoped></style>
+<script setup lang="js"></script>
+<style>
+.mozilla-headline-<uniquifier > {
+  font-family: 'Mozilla Headline', sans-serif;
+  font-optical-sizing: auto;
+  font-weight: <weight>;
+  font-style: normal;
+  font-variation-settings: 'wdth' 100;
+}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
@@ -7,7 +9,16 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [vue(), vueDevTools()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
